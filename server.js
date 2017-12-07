909550802a352f60d709e063d5b9c09a5dfc9660
Dev Dave <gewufiwuy@crymail2.com>
2017-12-07 19:07:20 +0530

    added new files
    Added files and connected to local server mongodb

---
+++ server.js
@@ -0,0 +1,35 @@
+var express = require('express'),
+app = express(),
+monk = require('monk');
+
+var swChars = monk('localhost:27017/starwars').get('character');
+
+app.use('/', express.static(__dirname + '/'));
+
+app.get('/',function(req,res){
+    res.sendFile(__dirname + '/index.html'); 
+});
+
+app.get('/character', function (req, res) {
+    swChars.find({}, function (err, docs) {
+        if (err == null) {
+            res.json(docs);
+        } else {
+            console.log(err);
+        }
+    });
+});
+
+app.get('/jedi', function (req, res) {
+    swChars.find({side: "Light"}, function (err, docs) {
+        if (err == null) {
+            res.json(docs);
+        } else {
+            console.log(err);
+        }
+    });
+});
+
+app.listen(3000, function () {
+    console.log("Server ready. Listening on port 3000");
+});
